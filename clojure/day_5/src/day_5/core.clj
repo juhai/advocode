73102d8ad267811984cabe7e5efe6a519adf530e
Juha Iso-Sipilä <juha.iso-sipila@iki.fi>
2020-12-08 21:34:22 +0000

    Day 5 second part solution in Clojure

--- clojure/day_5/src/day_5/core.clj
+++ clojure/day_5/src/day_5/core.clj
@@ -10,11 +10,21 @@
     (Integer/parseInt (str/replace (str/replace b-string #"[BR]" "1") #"[FL]" "0") 2)
   )
 
+(defn get-seat [seat-ids]
+  (let [sorted-ids (sort seat-ids)]
+    (/ (reduce + (first (filter
+      (fn [[p n]] (= (- n p) 2))
+      (map vector sorted-ids (rest sorted-ids)))
+    )) 2))
+  )
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [file-content (slurp "input.txt")
         lines (str/split file-content #"\n")]
     (println "Maximum seat ID is"
-             (apply max (map to-integer-foo lines))
-    )))
+             (apply max (map to-integer-foo lines)))
+    (println "My seat ID is"
+             (get-seat (map to-integer-foo lines)))
+    ))
