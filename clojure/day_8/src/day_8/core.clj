95cc5635accf1e9a22e51c60d02a9487c8808f07
Juha Iso-Sipilä <juha.iso-sipila@iki.fi>
2020-12-10 20:48:30 +0000

    Day 8, part II solution in Clojure

--- clojure/day_8/src/day_8/core.clj
+++ clojure/day_8/src/day_8/core.clj
@@ -10,27 +10,27 @@
     )
   )
 
-(defn get-accumulator-value [lines]
-  (let [program-lines (map get-program-line lines)
-        executed-lines (apply assoc {} (interleave (range (count lines)) (repeat 0)))]
-    (loop [executed-so-far executed-lines
-           accumulator 0
-           next-command-line 0]
-      (let [executed-now (update-in executed-so-far [next-command-line] inc)
-            current-program-line (nth program-lines next-command-line)
+(defn get-accumulator-value [executed-lines program-lines]
+  (loop [executed-so-far executed-lines
+         accumulator 0
+         next-command-line 0]
+    (let [executed-now (update-in executed-so-far [next-command-line] inc)
+          current-program-line (nth program-lines next-command-line)
+          accumulator-now
+          (+ accumulator (if (= (:op current-program-line) "acc") (:val current-program-line) 0))
+          new-next-command-line
+          (+ next-command-line
+             (cond
+             (= (:op current-program-line) "nop") 1
+             (= (:op current-program-line) "acc") 1
+             (= (:op current-program-line) "jmp") (:val current-program-line)))]
+      ;(println current-program-line accumulator-now new-next-command-line executed-now)
+      (cond (> (executed-now next-command-line) 1)
+            (- accumulator)
+            (= new-next-command-line (count executed-lines))
             accumulator-now
-            (+ accumulator (if (= (:op current-program-line) "acc") (:val current-program-line) 0))
-            new-next-command-line (+ next-command-line
-                                     (cond
-                                       (= (:op current-program-line) "nop") 1
-                                       (= (:op current-program-line) "acc") 1
-                                       (= (:op current-program-line) "jmp") (:val current-program-line)))
-            ]
-        ;(println current-program-line accumulator-now)
-        (if (> (executed-now next-command-line) 1)
-          accumulator
-          (recur executed-now accumulator-now new-next-command-line))
-        )
+            :else
+            (recur executed-now accumulator-now new-next-command-line))
       )
     )
   )
@@ -39,8 +39,23 @@
   "I don't do a whole lot ... yet."
   [& args]
   (let [file-content (slurp "input.txt")
-        lines (str/split file-content #"\n")]
-    (println "Part 1: Accumulator value is" (get-accumulator-value lines))
-    ;(println "Part 2: )
+        lines (str/split file-content #"\n")
+        program-lines (vec (map get-program-line lines))
+        executed-lines (apply assoc {} (interleave (range (count program-lines)) (repeat 0)))]
+    ; First part return negative answer that's filtered on the second round
+    (println "Part 1: Accumulator value is" (- (get-accumulator-value executed-lines program-lines)))
+    (println
+      "Part 2:"
+      (filter
+        pos?
+        (for [line (map-indexed vector program-lines)
+          :let [op (-> line second :op)
+                index (-> line first)
+                inv-op (cond (= op "jmp") "nop" (= op "nop") "jmp")]
+          :when (or (= op "jmp") (= op "nop"))]
+      (get-accumulator-value
+        executed-lines
+        (assoc program-lines index {:op inv-op :val (-> line second :val)})
+      )))
 
-    ))
+    )))
