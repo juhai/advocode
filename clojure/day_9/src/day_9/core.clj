fd0db5a441dd4df393545b24f95858f6fc8700c9
Juha Iso-Sipilä <juha.iso-sipila@iki.fi>
2020-12-10 22:21:42 +0000

    Day 9, part II solution in Clojure

--- clojure/day_9/src/day_9/core.clj
+++ clojure/day_9/src/day_9/core.clj
@@ -15,13 +15,37 @@
     )
   )
 
+(defn get-sum [lines target-value count-items]
+  (let [less-or-equal (take-while #(<= % target-value) (reductions + (take count-items lines)))
+        count-less-or-equal (count less-or-equal)
+        ]
+  (if (= (last less-or-equal) target-value)
+    {:match true :values (take count-less-or-equal lines)}
+    {:match false :values 'nil}
+  )))
+
+(defn find-target-sum [lines target-value]
+  (loop [index 0
+         work-lines lines]
+    (let [sum-found (first (filter #(true? (:match %))
+                           (map (partial get-sum work-lines target-value) (range 1 (count work-lines)))))]
+      (if (:match sum-found)
+        sum-found
+        (recur (inc index) (rest work-lines)))
+      ))
+  )
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [file-content (slurp "input.txt")
-        lines (map #(Long/parseLong %) (str/split file-content #"\n"))
+        lines (vec (map #(Long/parseLong %) (str/split file-content #"\n")))
         context-length 25]
-    (println (first (filter #(= (:result %) false)
-                            (map (partial check-if-sum-found lines context-length)
-                                 (range context-length (count lines))))))
-    ))
+    (let [answer1 (first (filter #(= (:result %) false)
+                                 (map (partial check-if-sum-found lines context-length)
+                                      (range context-length (count lines)))))]
+      (println "Part 1:" answer1)
+      (let [match (find-target-sum (take (:index answer1) lines) (:value answer1))]
+        (println "Part 2:" (+ (apply min (:values match)) (apply max (:values match))) match))
+
+    )))
